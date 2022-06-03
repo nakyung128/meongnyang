32d34ba4b15dfcf44d4ea114b2436859dc6f0489
seungmio <68382824+seungmio@users.noreply.github.com>
2022-06-03 17:54:16 +0900

    Walking_Standing Up&Down&right&left motion 구현

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -7,14 +7,74 @@ public class MovingObject : MonoBehaviour
     public float speed;
 
     private Vector3 vector;
-
+    
     public float runSpeed;
     private float applyRunSpeed;
+    private bool applyRunFlag = false;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    private bool canMove = true;
+
+    private Animator animator;
+
+    //speed * walkCount = pixel 단위
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();    //component 통제
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        while(Input.GetAxisRaw("Vertical")!=0 || Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+
+            else
+            {
+                applyRunSpeed = 0;
+                applyRunFlag = false;
+            }
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if (vector.x != 0)
+            {
+                //vector.x==1이나 -1 일 경우 vector.y=0 으로
+                vector.y = 0;
+            }
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while (currentWalkCount < walkCount)
+            {
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                }
+                if (applyRunFlag)
+                {
+                    currentWalkCount++;
+                }
+                currentWalkCount++;
+                yield return new WaitForSeconds(0.01f);    //0.01초동안 대기 (반복문 20번:0.2초)
+            }
+            currentWalkCount = 0;
+
+        }
+            animator.SetBool("Walking", false);
+            canMove = true;
     }
 
     // Update is called once per frame
@@ -26,23 +86,13 @@ public class MovingObject : MonoBehaviour
         // 상 방향키: 1 리턴, 하 방향키: -1 리턴
 
         // 좌, 우, 상, 하 방향키 눌렸을 경우
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if(canMove)
         {
-            if (Input.GetKey(KeyCode.LeftShift))
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
             {
-                applyRunSpeed = runSpeed;
-            }
-            else applyRunSpeed = 0;
+                canMove = false;    //coroutine 반복실행 방지
+                StartCoroutine(MoveCoroutine());    //coroutine 실행
 
-            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
-
-            if (vector.x != 0)
-            {
-                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
-            }
-            else if (vector.y != 0)
-            {
-                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
             }
         }
     }

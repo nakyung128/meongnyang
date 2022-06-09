6fdd6661b28336b01e38b2e3cec84827e1f2b0fc
seungmio <68382824+seungmio@users.noreply.github.com>
2022-06-09 10:31:17 +0900

    BGM 구현

---
+++ Assets/Scripts/TestScript2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript2 : MonoBehaviour
+{
+    BGMManager BGM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM = FindObjectOfType<BGMManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if...
+        StartCoroutine(final());
+        
+    }
+
+    IEnumerator final()
+    {
+        BGM.FadeOutMusic();
+
+        yield return new WaitForSeconds(3f);
+
+        BGM.FadeInMusic();
+    }
+}

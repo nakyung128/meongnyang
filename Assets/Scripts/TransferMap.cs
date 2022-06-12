deab5d073caa165cc18faf3dd2b9622e897c49bc
seungmio <68382824+seungmio@users.noreply.github.com>
2022-06-13 02:03:52 +0900

    테스트용 스크립트 수정

--- Assets/Scripts/TransferMap.cs
+++ Assets/Scripts/TransferMap.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TransferMap : MonoBehaviour
-{
+{//
     public string transferMapName;  //이동할 맵의 이름
     
     public Transform target;

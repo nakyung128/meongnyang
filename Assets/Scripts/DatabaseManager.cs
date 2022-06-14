caba6bf0b8af7caa54926f62f58135288bdd83f7
nakyung128 <lnk12888@naver.com>
2022-06-15 01:35:19 +0900

    아이템 구현

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -32,9 +32,16 @@ public class DatabaseManager : MonoBehaviour
     public string[] switch_name;
     public bool[] switches;
 
+    public List<Item> itemList = new List<Item>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        itemList.Add(new Item(10001, "첫 번쨰 키", "부숴진 열쇠의 첫 번째 조각", Item.ItemType.Equip, "key-0001_1"));
+        itemList.Add(new Item(10002, "두 번쨰 키", "부숴진 열쇠의 두 번째 조각", Item.ItemType.Equip, "key-0001_2"));
+        itemList.Add(new Item(10003, "첫 번쨰 키", "부숴진 열쇠의 세 번째 조각", Item.ItemType.Equip, "key-0001_3"));
+        itemList.Add(new Item(10004, "첫 번쨰 키", "완성된 열쇠", Item.ItemType.Use, "key-0001_0"));
+
     }
 }

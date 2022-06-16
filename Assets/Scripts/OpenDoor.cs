0d147ad16f221b52e6a593488337e8477e39051a
nakyung128 <lnk12888@naver.com>
2022-06-17 01:51:16 +0900

    열쇠로 문 열기 구현

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -1,62 +1,32 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-
-    public Dialogue dialogue_1;
-    public Dialogue dialogue_2;
-    public Inventory inventory;
+    [SerializeField]
+    public Dialogue dialogue1;
+    public Dialogue dialogue2;
 
     private DialogueManager theDM;
-    private OrderManager theOrder;
-    private PlayerManager thePlayer; // DirY = 1일 때 (위쪽 바라볼 때)
-    private FadeManager theFade;
-
-    private bool flag; // 한 번 보면 다시 못 보게 함. 이거는 나중에 뺄지 말지 얘기해 보아야 할 듯.
+    private Inventory inventory;
 
     // Start is called before the first frame update
     void Start()
     {
         theDM = FindObjectOfType<DialogueManager>();
-        theOrder = FindObjectOfType<OrderManager>();
-        thePlayer = FindObjectOfType<PlayerManager>();
-        theFade = FindObjectOfType<FadeManager>();
         inventory = GameObject.Find("Player").GetComponent<Inventory>();
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!flag && thePlayer.animator.GetFloat("DirY") == 1f) // 캐릭터가 위를 바라볼 때
+        if (collision.gameObject.name == "Player" && (!inventory.slots[0].isEmpty && !inventory.slots[1].isEmpty && !inventory.slots[2].isEmpty))
         {
-            flag = true; // 실행됨
-            StartCoroutine(EventCoroutine());
-        }
-    }
-
-    IEnumerator EventCoroutine()
-    {
-        theOrder.PreLoadCharacter(); // 오더매니저에 있는 리스트에 채워주어야 함
-        theOrder.NotMove(); // 움직이지 않도록
-
-        if (!inventory.slots[0].isEmpty && !inventory.slots[1].isEmpty && !inventory.slots[2].isEmpty)
-        {
-            theDM.ShowDialogue(dialogue_1);
+            theDM.ShowDialogue(dialogue1);
         }
         else
         {
-            theDM.ShowDialogue(dialogue_2);
+            theDM.ShowDialogue(dialogue2);
         }
-
-        // 대화가 끝날 때까지 기다렸다가 대화가 끝나면 이동시킬 것이다.
-        yield return new WaitUntil(() => !theDM.talking);
-
-        yield return new WaitUntil(() => thePlayer.queue.Count == 0);
-
-        //theFade.Flash(); 필요없음 번개 효과임
-
-        theOrder.Move();
     }
 }
